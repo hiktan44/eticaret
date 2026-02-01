99623f257c8f6b1ef6e4a9b18c32b25ba976f62d
ht <hikmet@texmart.com>
2026-02-02 01:52:45 +0300

    fix: public/env-config.js git'ten kaldırıldı
    - Netlify secrets scanner bu dosyayı tespit ediyordu
    - Dosya .gitignore'a eklendi
    - Netlify build script zaten runtime'da oluşturacak

--- public/env-config.js
+++
@@ -1,3 +0,0 @@
-// Local development environment config
-// Bu dosya production'da Netlify tarafından otom atik oluşturulacak
-window.ENV_VITE_GEMINI_API_KEY = "AIzaSyCc41M3nMQp2eyUujKeoQiEJb-jqrjWouE";
